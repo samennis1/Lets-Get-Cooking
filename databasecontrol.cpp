94f3e89124a6f88f3f781fb7df1d78e47b540d1d
Sam Ennis <samennis127@gmail.com>
2023-03-26 17:59:32 +0100

    Start Database Wrappers - Basically creating an ORM at this point

--- databasecontrol.cpp
+++ databasecontrol.cpp
@@ -13,14 +13,65 @@ void DatabaseControl::preSave() {
     cout << "Default presave";
 }
 
-bool DatabaseControl::save(string queryString) {
+DBRecordReturn DatabaseControl::execute(QString queryString) {
+    DBRecordReturn returnData;
     QSqlQuery saveQuery;
-    QString queryText = QString::fromStdString(queryString);
+    QString queryText = queryString;
     saveQuery.prepare(queryText);
+    qDebug() << "Executing... " << saveQuery.lastQuery();
 
     if(saveQuery.exec()) {
-        return true;
+        qDebug() << "Exec!";
+        returnData.returnedData = std::move(saveQuery);
+        returnData.success = true;
     } else {
-        return false;
+        qDebug() << "Nein!";
+        returnData.success = false;
+    }
+
+    return returnData;
+}
+
+DBResultField DatabaseControl::executeAndRetrieve(QString queryString, QString field) {
+    QSqlQuery saveQuery;
+    QString queryText = queryString;
+    saveQuery.prepare(queryText);
+
+    DBResultField result;
+
+    if(saveQuery.exec()) {
+        int idName = saveQuery.record().indexOf(field);
+        while(saveQuery.next()) {
+            QString fieldGet = saveQuery.value(idName).toString();
+            result.success = true;
+            result.result = fieldGet;
+        }
+    } else {
+        result.success = false;
+    }
+
+    return result;
+}
+
+
+DBConditionalReturn DatabaseControl::findOrCreate(QString preQuery, QString postQuery) {
+    DBConditionalReturn final;
+    DBRecordReturn valid = this->execute(preQuery);
+    if(!valid.success) {
+        final.firstStatementWasExecuted = false;
+        return final;
+    } else {
+        final.firstStatementWasExecuted = true;
+        final.firstQueryData = std::move(valid.returnedData);
+
+        if(final.firstQueryData.next()) {
+            final.firstQueryData.previous();
+            return final;
+        }
+
+        DBRecordReturn run = this->execute(postQuery);
+        final.secondStatementWasExecuted = run.success;
+        final.secondQueryData = std::move(run.returnedData);
+        return final;
     }
 }
--- databasecontrol.h
+++ databasecontrol.h
@@ -1,14 +1,37 @@
 #ifndef DATABASECONTROL_H
 #define DATABASECONTROL_H
 #include <string>
+#include "QtSql/QSqlQuery.h"
 #include "databasemanager.h"
+
+struct DBDefault {
+    bool success;
+};
+
+struct DBResultField : DBDefault {
+    QString result;
+};
+
+struct DBConditionalReturn {
+    bool firstStatementWasExecuted;
+    bool secondStatementWasExecuted;
+    QSqlQuery firstQueryData;
+    QSqlQuery secondQueryData;
+};
+
+struct DBRecordReturn : DBDefault {
+   QSqlQuery returnedData;
+};
 
 class DatabaseControl
 {
 public:
     static DatabaseManager db;
     DatabaseControl();
-    bool save(string);
+    DBRecordReturn execute(string);
+    DBRecordReturn execute(QString);
+    DBResultField executeAndRetrieve(QString, QString);
+    DBConditionalReturn findOrCreate(QString, QString);
     virtual void preSave();
 };
 
