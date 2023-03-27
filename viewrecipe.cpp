a6eaa8c8a488750bf8a3d46d7992b837f7a1f1d1
Sam Ennis <samennis127@gmail.com>
2023-03-27 15:46:00 +0100

    Load dietary restrictions into view

--- viewrecipe.cpp
+++ viewrecipe.cpp
@@ -3,6 +3,7 @@
 #include "recipe.h"
 #include <QVector>
 #include "qvector.h"
+#include <iostream>
 
 ViewRecipe::ViewRecipe(QWidget *parent, int id, QString name) :
     QDialog(parent),
@@ -14,11 +15,19 @@ ViewRecipe::ViewRecipe(QWidget *parent, int id, QString name) :
     qDebug() << viewedRecipe.getName();
     ui->recipeName->setText(viewedRecipe.getName());
     QVector<Ingredient> ingredientsRecipe(viewedRecipe.ingredients.begin(), viewedRecipe.ingredients.end());
+    qDebug() << "Ingredient SIZE " << ingredientsRecipe.size();
     this->displayList = new IngredientsList(nullptr, ingredientsRecipe);
     ui->ingredientsList->setModel(this->displayList);
+    //VVGDN
+    QList<QCheckBox*> list = {ui->veganBox, ui->vegBox, ui->glutenBox, ui->dairyBox, ui->nutBox};
 
-    for(int i = 0 ; i < 6; i++) {
-
+    for(int i = 0 ; i < list.size(); i++) {
+        int set = viewedRecipe.hasDietaryRestriction((DietaryRestriction) (i+1));
+        if(set) {
+            qDebug() << "Running " << i;
+            list.at(i)->setCheckable(true);
+            list.at(i)->setChecked(true);
+        }
     }
 }
 
